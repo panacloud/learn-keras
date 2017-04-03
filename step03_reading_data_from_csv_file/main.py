afcf0fb26a9935655e542575995b1ad2d35c5fea
Zia Khan <ziaukhan@hotmail.com>
2017-04-03 13:22:24 +0500

    working on step 3

--- step03_reading_data_from_csv_file/main.py
+++ step03_reading_data_from_csv_file/main.py
@@ -31,36 +31,6 @@ network.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['
 
 network.fit(train_data, train_labels, epochs=5, batch_size=128)
 
+test_loss, test_acc = network.evaluate(test_data, test_labels)
 
-# (train_images, train_labels), (test_images, test_labels) = mnist.load_data() # unpacking tuple of four numpy arrays
-#
-# network = Sequential()
-# network.add(Dense(512, activation='relu', input_shape=(28 * 28,)))
-# network.add(Dense(10, activation='softmax'))
-#
-# network.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
-#
-# train_images = train_images.reshape((60000, 28 * 28))
-# train_images = train_images.astype('float32') / 255
-# test_images = test_images.reshape((10000, 28 * 28))
-# test_images = test_images.astype('float32') / 255
-#
-# from keras.utils.np_utils import to_categorical
-# train_labels = to_categorical(train_labels)
-# test_labels = to_categorical(test_labels)
-#
-# network.fit(train_images, train_labels, epochs=5, batch_size=128)
-#
-# print("**************** Test *********************")
-#
-# test_loss, test_acc = network.evaluate(test_images, test_labels)
-#
-# print('test_acc:', test_acc)
-
-
-
-
-
-
-
-
+print('test_acc:', test_acc)
--- step03_reading_data_from_csv_file/readme.md
+++ step03_reading_data_from_csv_file/readme.md
@@ -1,3 +1,9 @@
+Data source:
+
+https://www.kaggle.com/c/GiveMeSomeCredit
+
+Read:
+
 https://docs.scipy.org/doc/numpy/user/basics.io.genfromtxt.html
 
 
